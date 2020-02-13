fb28046b5c092dcd36aa069df53d58c3f3347a8c
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-02-13 15:59:00 -0500

    Format date Kawara-esquely

--- components/Date.tsx
+++ components/Date.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
+import { format } from 'date-fns'
 
-export const KawaraDate: React.FC<{ date: Date }> = ({ date }) => (
-  <>
-    <section className="date">{date.toLocaleDateString()}</section>
-    <style jsx>
-      {`
-        @font-face {
-          font-family: 'kawara';
-          src: url('OnKawara-Regular.otf');
-        }
+export const KawaraDate: React.FC<{ date: Date }> = ({ date }) => {
+  const formattedDate = format(date, "MMM.d,Y")
+  return (
+    <>
+      <section className="date">{formattedDate}</section>
+      <style jsx>
+        {`
+          @font-face {
+            font-family: 'kawara';
+            src: url('OnKawara-Regular.otf');
+          }
 
-        section.date {
-          font-family: kawara;
-          font-size: 10vw;
-        }
-      `}
-    </style>
-  </>
-)
+          section.date {
+            font-family: kawara;
+            font-size: 10vw;
+          }
+        `}
+      </style>
+    </>
+  )
+}
